0fa4b20e298cf7797699bc3c90f9fc0f56ab3cf3
root <root@devmonitor.frogfoot.net>
2021-11-18 18:05:12 +0200

    Iris API testing first success to dev-iris machine.

--- iris-api-testing/test.sh
+++ iris-api-testing/test.sh
@@ -1,1 +1,20 @@
-#
+#!/usr/bin/env bash
+
+
+# Get list of Accedian LT-S devices from IRIS
+
+#response=$(curl --silent --location --globoff \
+#--request GET 'http://iris-dev.frogfoot.net/iris/api2/api/devices' \
+#--user 'cpt-backup01:Pga<3;9{cz' \
+#--data-urlencode 'search={"deleted":0,"hostname":{"like":"%-lts%"}}' | \
+#jq -r '.data[] | "\(.hostname) \(.poller_a) \(.poller_b)"')
+
+
+curl --silent --location --globoff --request GET 'http://iris-dev.frogfoot.net/iris/api2/api/devices' \
+--user 'nelius@frogfoot.com:Jy0x[w0G-A' \
+--data-urlencode 'search={"deleted":0,"hostname":{"like":"%-lts%"}}' | \
+jq -r '.data[] | "\(.hostname) \(.poller_a) \(.poller_b)"'
+
+
+
+
