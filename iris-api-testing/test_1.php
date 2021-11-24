773e011a3d0b112d42a4c779c7fcc8988fbe593e
nelius314159 <nelius314159@gmail.com>
2021-11-24 12:21:15 +0200

    Iris API dev attempting CURL to Iris prod - JM login success.

--- iris-api-testing/test_1.php
+++ iris-api-testing/test_1.php
@@ -1,21 +1,31 @@
 <?php
 
-//echo 'Curl: ', function_exists('curl_version') ? 'Enabled' : 'Disabled';
-//die;
-
-
-
 echo "Start.\n";
-
-
-//$URL = "https://iris.frogfoot.net/iris/api2/api/graphs?search.graphtype=snipsLatency&search.basetag.like=%25".$tag."%25";
-//echo $URL."\n";
-//die();
-
 
 //--- test basic Iris API
 $tag = "CXNK000CE9D9";
 
+$url = "https://iris.frogfoot.net/iris/api2/api/graphs?search.graphtype=snipsLatency&search.basetag.like=%25".$tag."%25";
+$curl = curl_init($url);
+                curl_setopt($curl, CURLOPT_TIMEOUT, 5);
+                curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($curl, CURLOPT_USERPWD, "jm@frogfoot.com:frog4get");
+                $JobJson = curl_exec($curl);
+                curl_close($curl);
+                var_dump($JobJson);
+
+
+die;
+
+
+
+
+
+
+
+//
 $curl = curl_init();
 curl_setopt_array($curl, array(
 
@@ -34,11 +44,30 @@ curl_setopt_array($curl, array(
 
 			       )
 		  );
-
 $response = curl_exec($curl);
-
 curl_close($curl);
 echo $response;
+
+
+//
+
+
+                // Get Graph number
+		                                $URL = "https://iris.frogfoot.net/iris/api2/api/graphs?search.graphtype=snipsLatency&search.basetag.like=%25".$Tag."%25";
+                //echo $URL."\n";
+		                                //die();
+                curl_setopt($CH, CURLOPT_TIMEOUT, 5);
+                curl_setopt($CH, CURLOPT_CONNECTTIMEOUT, 5);
+                curl_setopt($CH, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($CH, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($CH, CURLOPT_USERPWD, "jm@frogfoot.com:frog4get");
+                $JobJson = curl_exec($CH);
+                curl_close($CH);
+                var_dump($JobJson);
+
+
+//
+
 
 ///
 //curl --silent --location --globoff --request GET 'http://iris-dev.frogfoot.net/iris/api2/api/devices' \
